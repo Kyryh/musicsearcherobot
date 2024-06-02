ac5847efc5f36f298f2e3dff87ffdea6c7c77e2b
Kyryh <kyrykiller07@gmail.com>
2024-06-02 20:07:28 +0200

    Removed most features to start rework

--- __main__.py
+++ __main__.py
@@ -23,12 +23,6 @@ from telegram.ext import (
     PicklePersistence
 )
 
-import urllib3
-from yt_dlp import YoutubeDL, DownloadError
-
-from spotipy import Spotify
-from spotipy.oauth2 import SpotifyClientCredentials
-
 from os import getenv, remove
 import typing
 
@@ -45,23 +39,7 @@ logger = logging.getLogger(__name__)
 TOKEN = getenv("TOKEN")
 OWNER_USER_ID = getenv("OWNER_USER_ID")
 
-YTDL_OPTIONS = {
-    "outtmpl": "%(id)s.%(ext)s",
-    "format": "bestaudio",
-    "postprocessors": [
-        {
-            "key": "FFmpegExtractAudio",
-            "preferredcodec": "m4a"
-        }
-    ],
-    "format_sort": ["filesize:10M"],
-    "max_filesize": 50_000_000
-}
 
-spotify_client = Spotify(auth_manager=SpotifyClientCredentials(
-    getenv("SPOTIFY_CLIENT_ID"),
-    getenv("SPOTIFY_CLIENT_SECRET")
-))
 
 
 class FilesizeTooLargeException(Exception):
@@ -93,7 +71,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "Welcome to the bot!\n"
         "\n"
         "<b>Features</b>:\n"
-        "- Downloading songs via URL\n"
         "- Searching and downloading songs based on title and/or artists\n"
         "- Searching and downloading songs in inline mode\n"
         "\n"
@@ -102,21 +79,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         link_preview_options=LinkPreviewOptions(True),
         parse_mode="HTML"
     )
-
-async def handle_links(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    for url in update.effective_message.parse_entities([MessageEntity.URL, MessageEntity.TEXT_LINK]).values():
-        if "spotify.link" in url or "open.spotify.com" in url:
-            msg = await update.effective_chat.send_message("Spotify link detected, searching for song on Youtube Music...")
-            if "spotify.link" in url:
-                url = urllib3.request("HEAD", url).url
-            
-            spotify_song = spotify_client.track(url)
-            
-            await send_song_private(update.effective_chat, f"https://music.youtube.com/search?q={spotify_song['name']} {spotify_song['artists'][0]['name']}#songs", context)
-
-            await msg.delete()
-        else:
-            await send_song_private(update.effective_chat, url, context)
 
 async def handle_messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = await update.effective_chat.send_message(
@@ -155,7 +117,7 @@ async def send_song_private(chat: Chat, url: str, context: ContextTypes.DEFAULT_
             performer=info["performer"] if info else None,
             title=info["title"] if info else None,
             duration=info["duration"] if info else None,
-            thumbnail=(urllib3.request("GET", info["thumbnail"]).data if info["thumbnail"] else None) if info else None,
+            #thumbnail=(urllib3.request("GET", info["thumbnail"]).data if info["thumbnail"] else None) if info else None,
 
         )
         
@@ -175,7 +137,7 @@ async def download_song_button(update: Update, context: ContextTypes.DEFAULT_TYP
     await send_song_private(update.effective_chat, update.callback_query.data, context)
 
 def download_song(url: str):
-    with YoutubeDL(YTDL_OPTIONS| {'playlist_items': "1"}) as ydl:
+    """with YoutubeDL(YTDL_OPTIONS| {'playlist_items': "1"}) as ydl:
         info = None
         try:
             info = ydl.extract_info(url)
@@ -196,11 +158,12 @@ def download_song(url: str):
         "duration": song_info.get("duration"),
         "thumbnail": song_info["thumbnails"][0]["url"] if "thumbnails" in song_info else song_info.get("thumbnail"),
         "filename": ".".join(song_info["requested_downloads"][0]["filename"].split(".")[:-1])+".m4a"
-    }
+    }"""
+    return {}
 
 
 def search_songs(query: str, playlist_items: str):
-    with YoutubeDL(YTDL_OPTIONS | {'playlist_items': playlist_items, "extract_flat": True}) as ydl:
+    """with YoutubeDL(YTDL_OPTIONS | {'playlist_items': playlist_items, "extract_flat": True}) as ydl:
 
         info_songs = ydl.extract_info(f"https://music.youtube.com/search?q={query}#songs", download = False)
         info_videos = ydl.extract_info(f"https://music.youtube.com/search?q={query}#videos", download = False)
@@ -214,7 +177,8 @@ def search_songs(query: str, playlist_items: str):
                 "thumbnail": song["thumbnails"][0]["url"] if "thumbnails" in song else song.get("thumbnail")
             }
              for song in [item for sublist in zip(info_songs["entries"], info_videos["entries"]) for item in sublist] if (parse_duration(song["video_duration"]) or 0) <= 18000
-        ]
+        ]"""
+    return []
     
 
 async def inline_query(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -276,7 +240,7 @@ async def inline_query_edit(update: Update, context: ContextTypes.DEFAULT_TYPE):
             performer=song_info["performer"],
             title=song_info["title"],
             duration=song_info["duration"],
-            thumbnail=urllib3.request("GET", song_info["thumbnail"]).data if song_info["thumbnail"] else None
+            #thumbnail=urllib3.request("GET", song_info["thumbnail"]).data if song_info["thumbnail"] else None
         )).audio
         context.bot_data["cached_songs"][video_id] = audio
 
@@ -305,7 +269,6 @@ def main():
     )
 
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(MessageHandler(filters.Entity(MessageEntity.URL) | filters.Entity(MessageEntity.TEXT_LINK), handle_links))
     application.add_handler(MessageHandler(filters.TEXT, handle_messages))
 
     application.add_handler(CallbackQueryHandler(download_song_button))
